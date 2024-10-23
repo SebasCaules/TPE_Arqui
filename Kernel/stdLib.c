6c20bded44e2a70746d5d8a8a6978a6fe8fa4396
SebasCaules <sebastiancaules5@gmail.com>
2024-10-22 21:25:48 -0300

    changes

--- Kernel/stdLib.c
+++ Kernel/stdLib.c
@@ -8,11 +8,34 @@
 
 char mods[NOFMODS] = { 0 };
 
-int	printf(const char * str, ...){
+int	printf(const char * str, ...) {
+    char buffer[BUFFER_SIZE];
+    uint64_t length = 0;
     // Ej: printf("El numero es: %d +- %f. %c: %s", 12, 0.145, '$', "signo de pesos");
     for (uint64_t i = 0; str[i]; i++) {
-        
+        if(str[i] == '%') {
+            switch (str[i + 1]) {
+            case 'd':
+                buffer[length++];
+                break;
+            case 'f':
+                break;
+            case 'g':
+                break;
+            case 'c':
+                break;
+            case 's':
+                break;
+            
+            default:
+                break;
+            }
+            i++;
+        } else {
+            buffer[length++] = str[i];
+        }
     }
+    return sysCallHandler(4, STDOUT, buffer, length);
     
     
     
