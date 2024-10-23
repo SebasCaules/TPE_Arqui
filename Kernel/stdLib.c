d57f7ca22df1cb908aaa997a3a2d26279743afa0
SebasCaules <sebastiancaules5@gmail.com>
2024-10-22 21:12:10 -0300

    Update stdLib.c

--- Kernel/stdLib.c
+++ Kernel/stdLib.c
@@ -2,9 +2,20 @@
 #include <stdarg.h>
 #include "syscalls.h"
 
+#define NOFMODS 5
+#define LOWEST_MODIFIER 99
+#define BUFFER_SIZE 1024
+
+char mods[NOFMODS] = { 0 };
+
 int	printf(const char * str, ...){
     // Ej: printf("El numero es: %d +- %f. %c: %s", 12, 0.145, '$', "signo de pesos");
+    for (uint64_t i = 0; str[i]; i++) {
         
+    }
+    
+    
+    
 }
 
 int	 scanf(const char * __restrict, ...) {
