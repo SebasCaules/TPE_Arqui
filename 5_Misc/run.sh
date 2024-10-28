ab520b0b3e05839090df9e98358de2f11ca6735a
Alexishvegas <30813519+Alexishvegas@users.noreply.github.com>
2024-10-28 16:22:51 -0300

    snake game grid, fixed sound

--- 5_Misc/run.sh
+++ 5_Misc/run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-qemu-system-x86_64 -hda 1_Image/x64BareBonesImage.qcow2 -m 512
+#qemu-system-x86_64 -hda 1_Image/x64BareBonesImage.qcow2 -m 512
+
+
+## MACOS
+qemu-system-x86_64 -hda 1_Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=speaker -machine pcspk-audiodev=speaker
